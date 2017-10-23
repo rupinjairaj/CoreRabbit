68e5185d810436f48125b7714a4f62e941f725b7
Rupin Jairaj <rupinjairaj@gmail.com>
2017-10-23 22:23:16 +0530

    Initial Commit

---
+++ 3 PublishSubscribe/ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ReceiveLogs
+{
+    class ReceiveLogs
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare
+                (
+                    exchange: "logs",
+                    type: "fanout"
+                );
+
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind
+                (
+                    queue: queueName,
+                    exchange: "logs",
+                    routingKey: ""
+                );
+
+                Console.WriteLine(" [*] Waiting for logs.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] {0}", message);
+                };
+
+                channel.BasicConsume
+                (
+                    queue: queueName,
+                    autoAck: true,
+                    consumer: consumer
+                );
+
+                Console.WriteLine(" Press [enter] to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
